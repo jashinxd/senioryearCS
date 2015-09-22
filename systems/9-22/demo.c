94aa58cc2c9d9ae0ee0f1b9bcc68782a758a6161
Jason Shin <jasonshin98@gmail.com>
2015-09-22 11:55:53 -0400

    9-22

---
+++ systems/9-22/demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#inclide <stdlib.h>
+int main() {
+  
+  int bob = 7;
+  int a[10];
+  int fred = 92;
+
+  printf("bob = %d\n", bob);
+  printf("fred = %d\n", fred);
+  a[0] = 10;
+  printf("a[0] = %d\n", a[0]);
+  printf("a[-1] = %d\n", a[-1]);
+
+}
