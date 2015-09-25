eb49ff36d224f0c03455cdcc3f83dc7d37807179
Jason Shin <jasonshin98@gmail.com>
2015-09-25 05:47:42 -0400

    notes

--- systems/9-22/demo.c
+++ systems/9-22/demo.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#inclide <stdlib.h>
+#include <stdlib.h>
 int main() {
   
   int bob = 7;
   int a[10];
   int fred = 92;
+  int *pfred;
 
   printf("bob = %d\n", bob);
   printf("fred = %d\n", fred);
   a[0] = 10;
   printf("a[0] = %d\n", a[0]);
   printf("a[-1] = %d\n", a[-1]);
-
+  printf("a[-2] = %d\n", a[-2]);
+  printf("address of fred: %lu\n", &fred);
+  printf("pfred = %lu\n", pfred);
+  printf("address of pfred: %lu\n", &pfred);
 }
