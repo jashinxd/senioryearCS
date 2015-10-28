814b15bab1f5636ad9ddb6fc90669c2fe54d6b7d
Jason Shin <jasonshin98@gmail.com>
2015-10-28 11:45:23 -0400

    notes

---
+++ systems/10-28/stat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib,h>
+
+#include <sys/stat.h>
+
+int main() {
+
+  struct stat sb1;
+  struct stat sb2;
+  
+  stat( "10");
+
+  printf("size: %lu\tuser: %s\tmode: %o\tmtime: %", )
+
+  return 0;
+}
