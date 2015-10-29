bc14374eeff67daa0abfee1f5d866935af85db4b
Jason Shin <jasonshin98@gmail.com>
2015-10-29 11:48:38 -0400

    notes

--- systems/10-28/stat.c
+++ systems/10-28/stat.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdlib,h>
+#include <stdlib.h>
 
 #include <sys/stat.h>
 
 int main() {
 
+  //printf("works");
   struct stat sb1;
-  struct stat sb2;
+  //struct stat sb2;
+  stat("pizza", &sb1);
+  printf("size: %lu\tuser: %s\tmode: %o\tmtime: %s", sb1.st_size, sb1.st_uid, sb1.st_mode, ctime(&(sb1.st_mtime)));
+
   
-  stat( "10-21/chinesefood");
-
-  printf("size: %lu\tuser: %s\tmode: %o\tmtime: %", )
-
   return 0;
 }
