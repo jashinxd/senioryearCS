9e39f5f989648b8e12a217ad9119e5fd9603c5fe
Jason Shin <jasonshin98@gmail.com>
2016-02-04 14:03:42 -0500

    first image

---
+++ graphics/hw1/hw1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fnctl.h>
+
+int main() {
+  int fd = open( img.ppm, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+  char * img;
+  sprintf(img, %s, "P3\n500 500 255");
+  write(fd, img, strlen(line));
+  int i = 0;
+  int j = 0;
+  for (i = 0; i < 500; i++) {
+    for (j = 0; j < 500; j++) {
+      char r = ;
+      char g;
+      char b;
+    }
+  }
+}
